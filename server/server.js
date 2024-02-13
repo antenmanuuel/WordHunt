6eca684791fa63b2d098f4d3a97aba61c8653e62
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-02-13 13:47:16 -0500

    change server connection from local to cloud

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const http = require('http');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser')
-const handleSocketConnection = require('./socket');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const handleSocketConnection = require("./socket");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 
-
-mongoose.connect('mongodb://127.0.0.1/wordHunt'); //changed to ipv4
+mongoose.connect(
+  "mongodb+srv://antenmanuuel:anten2001@cluster0.aaqyrxt.mongodb.net/test"
+); //changed to ipv4
 
 const db = mongoose.connection;
-db.on("error", err => console.error(err));
-db.once("open", () => console.log('Connected to Database'))
+db.on("error", (err) => console.error(err));
+db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,10 +24,10 @@ app.use(cookieParser());
 // for making requests from client side application
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 // import routers
 const gamesRouter = require("./routers/games-router");
@@ -36,11 +37,10 @@ const userRouter = require("./routers/users-router");
 app.use("/games", gamesRouter);
 app.use("/users", userRouter);
 
-
 // socket handlers
 handleSocketConnection(server);
 
 // start server
 server.listen(3000, () => {
-    console.log("Server listening on port 3000");
+  console.log("Server listening on port 3000");
 });
