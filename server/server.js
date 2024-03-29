2f2cb4de0787358b4efb9e1eeefdc3ee424706e5
Progga <proggaparomita.dutta@stonybrook.edu>
2024-03-28 23:07:47 -0400

    Deployment 9

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
-// require("dotenv").config();
-
 const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const handleSocketConnection = require("./socket");
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
+
 const app = express();
 const server = http.createServer(app);
+const PORT = process.env.PORT;
 
-mongoose.connect(
-  "mongodb+srv://antenmanuuel:anten2001@cluster0.aaqyrxt.mongodb.net/test"
-); //changed to ipv4
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+mongoose.connect(process.env.MONGODB_URI); //changed to ipv4
+
+// "mongodb+srv://antenmanuuel:anten2001@cluster0.aaqyrxt.mongodb.net/test"
+
+// All other routes should redirect to the Vite app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+});
 
 const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
@@ -20,17 +27,14 @@ db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
-
 
 // for making requests from client side application
 app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://wordhunt-fff9a57fb464.herokuapp.com/"
+  );
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
@@ -47,10 +51,6 @@ app.use("/users", userRouter);
 handleSocketConnection(server);
 
 // start server
-// server.listen(3000, () => {
-//   console.log("Server listening on port 3000");
-// });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log("Server listening on port 3000");
 });
